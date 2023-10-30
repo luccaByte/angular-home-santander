d0f57a749ad63b2e4dc0b716f0df85af80b43a37
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2023-10-30 20:22:20 +0000

    add accountDataModel para banco fake

--- src/app/components/card-box/card-box.component.ts
+++ src/app/components/card-box/card-box.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AccountDataModel } from 'src/app/model/accountDataModel';
 
 @Component({
   selector: 'app-card-box',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-box.component.css']
 })
 export class CardBoxComponent {
+  accountData: AccountDataModel = {
+    name: "Lucas Gonçalves",
+    account: {
+      agency: "0002",
+      number: "00.000000-0"
+    },
+    card: {
+      limit: 1000,
+      number: "0001"
+    }
+  };
+
 
 }
