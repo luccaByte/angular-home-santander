0bad3033e44daad2e4bbe0364c4642d268e5b3e8
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2023-10-31 20:16:19 +0000

    implementando services, gerando outro modelo de dados dentro do model, construindo amount-box

--- src/app/components/card-box/card-box.component.ts
+++ src/app/components/card-box/card-box.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AccountDataModel } from 'src/app/model/accountDataModel';
+import { CardsService } from 'src/app/services/cards.service';
 
 @Component({
   selector: 'app-card-box',
   templateUrl: './card-box.component.html',
   styleUrls: ['./card-box.component.css']
 })
-export class CardBoxComponent {
+export class CardBoxComponent implements OnInit {
+
+  constructor(private service:CardsService) {
+
+  }
+
   accountData: AccountDataModel = {
     name: "Lucas Gonçalves",
     account: {
@@ -19,5 +25,17 @@ export class CardBoxComponent {
     }
   };
 
-
+  ngOnInit(): void {
+    this.getAccountData();
+  }
+  //Pegando name da api
+  getAccountData() {
+    this.service.getCard().subscribe( data => {
+      this.accountData.name = data.name;
+      this.accountData.account.agency = data.account.agency;
+      this.accountData.card.limit = data.card.limit;
+      this.accountData.account.number = data.account.number;
+      this.accountData.card.number = data.card.number.split("   ")[3]
+    })
+  }
 }
---
+++ src/app/services/accounts.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environments';
+
+@Injectable({
+	providedIn: 'root'
+})
+
+export class AccountsService {
+
+	private apiUrl = environment.API_URL;
+
+	constructor(private http: HttpClient) { }
+
+	getAccount(): Observable<any> {
+		return this.http.get<any>(this.apiUrl)
+	}
+}
