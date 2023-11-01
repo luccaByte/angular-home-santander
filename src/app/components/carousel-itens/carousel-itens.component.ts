9a3a8226e92154e6bca4cdd70bfc8bfb81896140
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2023-11-01 16:11:38 +0000

    construindo itens do carousel

--- src/app/components/carousel-itens/carousel-itens.component.ts
+++ src/app/components/carousel-itens/carousel-itens.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FeaturesService } from 'src/app/services/features.service';
+
 
 @Component({
-  selector: 'app-carousel-itens',
-  templateUrl: './carousel-itens.component.html',
-  styleUrls: ['./carousel-itens.component.css']
+	selector: 'app-carousel-itens',
+	templateUrl: './carousel-itens.component.html',
+	styleUrls: ['./carousel-itens.component.css']
 })
-export class CarouselItensComponent {
+export class CarouselItemsComponent implements OnInit {
+	items: { description: string; icon: string }[] = [];
+	visibleItems: { description: string; icon: string }[] = [];
+	activeIndex: number = 0;
 
+	constructor(private service: FeaturesService) { }
+
+	async ngOnInit() {
+		this.getFeatures();
+		console.log(this.items)
+	}
+
+	getFeatures() {
+		this.service.getFeature().subscribe(result => {
+			this.items = result.features;
+			this.updateVisibleItems();
+		})
+	}
+
+	async updateVisibleItems() {
+		this.visibleItems =
+			this.items.slice(this.activeIndex, this.activeIndex + 3);
+	}
+
+	moveLeft() {
+		if (this.activeIndex > 0) {
+			this.activeIndex--;
+			this.updateVisibleItems();
+		}
+	}
+
+	moveRight() {
+		if (this.activeIndex < this.items.length - 3) {
+			this.activeIndex++;
+			this.updateVisibleItems();
+		}
+	}
 }
